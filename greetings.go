ec877ce82e77614943662f151bbf42aa33ee9e03
Jonay Medina <jmedina@tribalworldwide.gt>
2025-01-16 13:06:59 -0400

    :sparkles: added errors

--- greetings.go
+++ greetings.go
@@ -1,12 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v! desde greetings", name)
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name is required")
+	}
+	message := fmt.Sprintf("Hello, %v! desde greetings", name)
+	return message, nil
 }
 
-func Hello2(name string) string {
-	return fmt.Sprintf("Hello, %v! desde greetings2", name)
+func Hello2(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name is required")
+	}
+	message := fmt.Sprintf("Hello, %v! desde greetings2", name)
+	return message, nil
 }
-
